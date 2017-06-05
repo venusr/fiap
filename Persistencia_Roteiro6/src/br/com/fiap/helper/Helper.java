0be7b157d555023b9a8327c1855f812c206e10a2
venusr <venus_rodrigues@yahoo.com.br>
2017-06-05 20:14:59 -0300

    Add files via upload

---
+++ Persistencia_Roteiro6/src/br/com/fiap/helper/Helper.java
@@ -0,0 +1,45 @@
+package br.com.fiap.helper;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.fiap.entity.Funcionario;
+
+public class Helper {
+	private EntityManager em;
+	public Helper(EntityManager em){
+		this.em = em;
+	}
+	public void salvar(Funcionario funcionario) throws Exception {
+		try {
+			
+			em.getTransaction().begin();
+			em.persist(funcionario);
+			em.getTransaction().commit();
+		}
+		catch (Exception e) {
+			throw e;
+		} finally {
+			em.close();
+		}
+	}
+	//JPQL: Usando Query
+	public List<Funcionario> listarFuncionarios(){
+		Query query = em.createQuery("select f from Funcionario f");
+		return query.getResultList();
+	}
+	
+	public Funcionario buscarFuncionario(String numMatricula){
+		Query query = em.createQuery("select f from Funcionario f where matricula = :matricula");
+				query.setParameter("matricula", numMatricula);
+				Funcionario f = (Funcionario)query.getSingleResult();
+				return f;
+	}
+	//JPQL: usando NamedQuery
+	public List<Funcionario> listarTodos(){
+		Query query = em.createNamedQuery("Funcionario.findAll");
+		return query.getResultList();
+	} 
+}	
